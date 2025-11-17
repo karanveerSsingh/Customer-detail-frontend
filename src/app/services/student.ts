5bfa1f98e4806cd225dd024a280aff43fb45cef7
karanveerSsingh <karanveer0508singh@gmail.com>
2025-11-17 20:58:39 +0530

    Deploy

--- src/app/services/student.ts
+++ src/app/services/student.ts
@@ -43,11 +43,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map } from 'rxjs';
+import { environment } from '../../environments/environment';
+
 
 @Injectable({ providedIn: 'root' })
 export class StudentService {
-  private apiUrl = 'http://localhost:3000/api/customers';
+  // private apiUrl = 'http://localhost:3000/api/customers';
   // private apiUrl = 'http://192.168.1.27:3000/api/customers';
+
+  private apiUrl = environment.baseApiPath;
 
   constructor(private http: HttpClient) {}
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,9 @@
 // Angular environment
 export const environment = {
-  production: false,
-  baseApiPath: 'http://localhost:3000/api/customers',
+  production: true,
+  // baseApiPath: 'http://localhost:3000/api/customers',
+    baseApiPath:  'http://localhost:3000/api/customers',
+  // baseApiPath: 'http://192.168.1.27:3000/api/customers',
+  // baseApiPath: 'https://customer-detail-backend-angular.onrender.com/api/customers',
+
 };
